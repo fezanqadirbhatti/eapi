7ddfd0776a10fbbe9fad97d7d8ed4bc135497105
fezanbhatti <fezanqadirbhatti@gmail.com>
2019-03-09 01:42:35 +0500

    Product and Review Models, Resource Controllers, Factories and migrations added

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/products','ProductController')->middleware('auth:api');
+Route::middleware('auth:api')->prefix('products')->group(function() {
+    Route::apiResource('/{product}/reviews','ReviewController');
+});
